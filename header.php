7a4c52f3fd84efdfbf15dd47fb2d03285d697bdc
Adomas Daškevičiaus <adomasda@gmail.com>
2024-09-02 17:43:58 +0300

    wtf is a purpose wp_enqueue_script wp_enqueue_style bs

--- header.php
+++ header.php
@@ -16,6 +16,11 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.3/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
 
 	<?php wp_head(); ?>
 </head>
@@ -25,6 +30,63 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
 
+	<div class="dropdown">
+  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+    Dropdown
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li role="separator" class="divider"></li>
+    <li><a href="#">Separated link</a></li>
+  </ul>
+</div>
+
+<!-- 	<div class="announement-bar pt-2 pb-2">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-4">
+
+					<ul class="announement-bar__list">
+						<li>
+							<i class="bi bi-telephone rounded-circle"></i>
+							<a href="tel: +44 555 22211" class="text-decoration-none">+44 555 22211</a>
+						</li>
+
+						<li>
+							<i class="bi bi-envelope rounded-circle"></i>
+							<a href="mailto:hello@pawsgang.shop" class="text-decoration-none">hello@pawsgang.shop</a>
+						</li>
+					</ul>
+
+				</div>
+
+				<div class="col-md-8 d-flex justify-content-end">
+
+					<ul class="announement-bar__list">
+							<li>
+								<i class="bi bi-truck rounded-circle"></i> FREE EU SHIPPING
+							</li>
+
+							<li>
+								<i class="bi bi-clock-history rounded-circle"></i> 30 DAYS MONEYBACK GUARANTEE
+							</li>
+							
+							<li>
+								<i class="bi bi-person rounded-circle"></i> 24/7 CUSTOMER SUPPORT
+							</li>
+							
+					</ul>
+
+				</div>
+
+			</div>
+		</div>
+	</div>
+ -->
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
