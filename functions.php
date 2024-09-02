1716208685619d2b35821ff18aa2b5d82fc7d900
Adomas Daškevičiaus <adomasda@gmail.com>
2024-09-02 19:46:17 +0300

    added back scss, why comp times are so high

--- functions.php
+++ functions.php
@@ -140,8 +140,8 @@ add_action( 'widgets_init', '_s_widgets_init' );
 function _s_scripts() {
 	wp_enqueue_style( '_s-style', get_stylesheet_uri(), array(), _S_VERSION );
 
-	wp_enqueue_style( 'bootstrap', get_template_directory_uri(). '/bootstrap/dist/css/bootstrap.min.css');
-	wp_enqueue_style( '_s-main', get_template_directory_uri(). '/css/main.css');
+	// wp_enqueue_style( 'bootstrap', get_template_directory_uri(). '/bootstrap/dist/css/bootstrap.min.css');
+	wp_enqueue_style( '_s-main', get_template_directory_uri(). '/css/gen/main.css');
 
 	wp_style_add_data( '_s-style', 'rtl', 'replace' );
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
